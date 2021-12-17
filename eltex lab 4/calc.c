db1d2a03e9b3401c898e208912be61e4050033ee
Napwob <47683903+Napwob@users.noreply.github.com>
2021-12-17 17:03:31 +0700

    Add files via upload

---
+++ eltex lab 4/calc.c
@@ -0,0 +1,23 @@
+int plus(int a, int b)
+{
+	return a+b;
+}
+
+int minus(int a, int b)
+{
+	return a-b;
+}
+
+
+int devide(int a, int b)
+{
+	return a/b;
+}
+
+int multiplication(int a, int b)
+{
+	return a*b;
+}
+
+
+
